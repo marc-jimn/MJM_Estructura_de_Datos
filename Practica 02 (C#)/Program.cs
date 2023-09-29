7546f6c017c0dd3f047cee2d7911da6a6909ab3a
markhipooh <marcela.jimenez15unach.mx>
2023-09-29 19:35:17 -0400

    Practica 02 - Sumatoria de 8 digitos enteros (C#)

---
+++ Practica 02 (C#)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    const int NUM = 8;
+    static void Main()
+    {
+        int[] nums = new int[NUM];
+        int total = 0;
+        for (int i = 0; i < NUM; i++)
+        {
+            Console.Write("Por favor, Introduzca el numero: ");
+            nums[i] = int.Parse(Console.ReadLine());
+            total += nums[i];
+        }
+        Console.WriteLine("\nEl total de numeros es: " + total);
+    }
+}
