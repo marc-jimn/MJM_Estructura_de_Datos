2b47c3dbfd174d3ae8862876b92912697c60d6d8
markhipooh <marcela.jimenez15unach.mx>
2023-09-29 20:51:15 -0400

    Práctica 03 - Ejemplos de Arreglos (C#)

---
+++ Practica 03 (C#)/Program.cs
@@ -0,0 +1,36 @@
+﻿// Marcela Jiménez Méndez
+// Programa 03: Arreglos unidimensionales
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[6] { 10, 20, 30, 40, 50, 60 };
+        int[] n = new int[3] { 3, 4, 5 };
+        char[] c = new char[4] { 'L', 'u', 'i', 's' };
+        String s = "Mona Lisa";
+        
+        Console.Write("\n - ");
+        foreach (int i in numeros)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.Write("\n - ");
+        foreach (int i in n)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.Write("\n - ");
+        foreach (char i in c)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.Write("\n - ");
+        Console.Write(s);
+        Console.Write("\n");
+    }
+}
