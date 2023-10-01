560f424bbf6ee291586e129fbe9df3ecc166cb5e
markhipooh <marcela.jimenez15unach.mx>
2023-10-01 14:03:15 -0400

    Práctica 04 - Arrelgos Multidimensionales: Recorrido de una Matriz nxm (C#)

---
+++ Practica 04 (C#)/Program.cs
@@ -0,0 +1,39 @@
+﻿// Marcela Jimenez Mendez
+// Programa 04: Arreglo Bidimensional
+
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        const int maxf = 3;
+        const int maxc = 5;
+
+        int[,] a = new int[maxf, maxc];
+
+        // leer el array
+
+        Console.Write("\n");
+        for (int i = 0; i < maxf; i++)
+        {
+            for (int j = 0; j < maxc; j++)
+            {
+                Console.Write(" - Introduce un numero: ");
+                int num = int.Parse(Console.ReadLine());
+                a[i, j] = num;
+            }
+        }
+
+        Console.Write("\n");
+        for (int i = 0; i < maxf; i++)
+        {
+            for (int j = 0; j < maxc; j++)
+            {
+                Console.Write(a[i, j]);
+                Console.Write(" ");
+            }
+            Console.Write("\n");
+        }
+    }
+}
