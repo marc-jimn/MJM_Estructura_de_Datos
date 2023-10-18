84ee5def607c6e35f34c2c25ed95fa3eb6d9fb41
markhipooh <marcela.jimenez15unach.mx>
2023-10-18 19:24:59 -0400

    Práctica 07 -  Pila (C#)

---
+++ Practica 07 (C#)/Program.cs
@@ -0,0 +1,53 @@
+﻿// Marcela Jimenez Mendez
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        const int capacidad = 10;
+        int[] elementos = new int[capacidad];
+        int tope = -1;
+
+        Console.WriteLine(" Teclea elemento de la pila: (termina con -1)");
+        int x = 0;
+        const int CLAVE = -1;
+
+        while (x != CLAVE)
+        {
+            try
+            {
+                x = int.Parse(Console.ReadLine());
+                if (tope < capacidad - 1)
+                {
+                    tope++;
+                    elementos[tope] = x;
+                }
+                else
+                {
+                    Console.WriteLine(" Excepcion: Pila Llena");
+                    break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(" Excepcion: Entrada no valida");
+            }
+        }
+
+        if (tope >= 0)
+        {
+            Console.Write(" Elementos de la Pila: ");
+            while (tope >= 0)
+            {
+                x = elementos[tope];
+                tope--;
+                Console.Write(x + " ");
+            }
+        }
+        else
+        {
+            Console.WriteLine(" Pila Vacia");
+        }
+    }
+}
