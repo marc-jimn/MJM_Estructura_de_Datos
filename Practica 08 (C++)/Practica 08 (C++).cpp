8637e516d9eff7897de999af59b526ab58205178
markhipooh <marcela.jimenez15unach.mx>
2023-10-19 21:53:38 -0400

    Practica 08 - Colas (Python)

---
+++ Practica 08 (C++)/Practica 08 (C++).cpp
@@ -0,0 +1,69 @@
+// Marcela Jimenez Mendez
+
+#include <iostream>
+#define MaxTamC 10
+
+typedef int TipoDato;
+
+using namespace std;
+
+int main() {
+	int frente = 0, final = 0;
+	TipoDato A[MaxTamC];
+	int contador = 0;
+
+	// Inicializar la cola
+	frente = 0;
+	final = 0;
+
+	cout << " Desea agregar elementos a la cola? (s/n): ";
+	char respuesta;
+	cin >> respuesta;
+
+	while ((respuesta == 's' || respuesta == 's') && contador < 10) {
+		if ((final + 1) % MaxTamC == frente) {
+			cout << " Desbordamiento de la cola" << endl;
+			return 1;
+		}
+
+		cout << " Ingrese un elemento para la cola: ";
+		TipoDato elemento;
+		cin >> elemento;
+		
+		final = (final + 1) % MaxTamC;
+		A[final] = elemento;
+
+		contador++;
+		cout << " Elemento " << contador << " agregado a la cola: " << elemento << endl;
+
+		if (contador < 10) {
+			cout << " Desea agregar mas elementos a la cola? (s/n): ";
+			cin >> respuesta;
+		}
+	}
+
+	// Validar si la cola está vacia
+	if (frente == final) {
+		cout << " La cola esta vacia: " << endl;
+		return 1;
+	}
+
+	// Obtener el primer elemento de la cola
+	TipoDato primerElemento = A[(frente + 1) % MaxTamC];
+	cout << " El primer elemento de la cola es: " << primerElemento << endl;
+
+	// Eliminar un elemento de la cola
+	frente = (frente + 1) & MaxTamC;
+
+	// Imprimir elementos de la cola en el orden en que fueron ingresados
+	cout << " Elementos de la cola en el orden de ingreso: " << endl;
+	for (int i = frente + 1; i <= final; i = (i + 1) % MaxTamC) {
+		cout << A[i] << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
+
+
